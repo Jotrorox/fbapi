dd56c547d2fe8c29d6232a54ed5db08ef637ea83
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-08 22:57:08 +0200

    Adding Docker

--- src/main.js
+++ src/main.js
@@ -103,7 +103,7 @@ app.post('/fizzbuzz/file/html', upload.single('file'), async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
