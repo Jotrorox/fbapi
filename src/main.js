0f08c4127a18519a14836bfc6efd94cba9716967
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-08 21:10:08 +0200

    Updating the package.json

--- src/main.js
+++ src/main.js
@@ -59,7 +59,7 @@ app.post('/fizzbuzz/file', upload.single('file'), async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
