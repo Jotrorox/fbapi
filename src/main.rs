0af97b9ec8ce01d065e1e89f8da28ca44dd55bc2
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-09 15:12:38 +0200

    Adding Documentation

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
-use rocket::{http::ContentType, serde::json::Json, Data};
-use std::io::{self};
-use crate::rocket::tokio::io::AsyncReadExt;
+// Importing the JSON module from Rocket's serde package for JSON serialization and deserialization
+use rocket::serde::json::Json;
 
+// Macro to enable external crates like Rocket
 #[macro_use] extern crate rocket;
 
+// Deriving Responder for automatic conversion to HTTP response
 #[derive(Responder)]
-#[response(status = 400, content_type = "json")]
-struct ErrorResponse {
-    error: String,
-}
-
-#[derive(Responder)]
+// Specifying the default status code and content type for responses of this type
 #[response(status = 200, content_type = "json")]
 struct SuccessResponse<T: rocket::serde::Serialize> {
+    // Wrapping the data in a Json object for serialization
     data: Json<T>,
 }
 
+// Deriving Serialize for automatic conversion to JSON
 #[derive(serde::Serialize)]
 struct FizzBuzzResult {
+    // Fields to store the input number and the computed FizzBuzz result
     number: i32,
     result: String,
 }
 
+// Handler for the root path ("/")
 #[get("/")]
 fn hello() -> &'static str {
-    "Hello, world!"
+    // Returning a static string with information about the API
+    "This is a FizzBuzz API. Try /fizzbuzz/<number>. For more information, visit https://github.com/Jotrorox/fbapi."
 }
 
+// Handler for the "/fizzbuzz/<number>" path, capturing the number as an i32
 #[get("/fizzbuzz/<number>")]
 fn fizzbuzz_single(number: i32) -> SuccessResponse<FizzBuzzResult> {
+    // Computing the FizzBuzz result for the given number
     let result = fizzbuzz(number);
+    // Creating a SuccessResponse with the FizzBuzzResult
     SuccessResponse {
         data: Json(FizzBuzzResult {
             number,
@@ -38,21 +42,22 @@ fn fizzbuzz_single(number: i32) -> SuccessResponse<FizzBuzzResult> {
     }
 }
 
+// Utility function to compute the FizzBuzz result for a given number
 fn fizzbuzz(number: i32) -> String {
-    if number % 3 == 0 && number % 5 == 0 {
-        "FizzBuzz".into()
-    } else if number % 3 == 0 {
-        "Fizz".into()
-    } else if number % 5 == 0 {
-        "Buzz".into()
-    } else {
-        number.to_string()
+    // Using pattern matching to determine the FizzBuzz result based on divisibility
+    match (number % 3, number % 5) {
+        (0, 0) => "FizzBuzz".into(), // Divisible by both 3 and 5
+        (0, _) => "Fizz".into(),     // Divisible by 3 but not 5
+        (_, 0) => "Buzz".into(),     // Divisible by 5 but not 3
+        _ => number.to_string(),     // Not divisible by either 3 or 5
     }
 }
 
-
+// Entry point for launching the Rocket application
 #[launch]
 fn rocket() -> _ {
+    // Building and configuring the Rocket instance
     rocket::build()
-        .mount("/", routes![hello, fizzbuzz_single])
+        // Mounting routes for the handlers
+       .mount("/", routes![hello, fizzbuzz_single])
 }
