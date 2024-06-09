fb15849314705687035d7de294372bbb7804eb14
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-09 14:27:41 +0200

    Adding more dependencies and the fizzbuzz route

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,56 @@
+use rocket::serde::json::Json;
+
 #[macro_use] extern crate rocket;
+
+#[derive(Responder)]
+#[response(status = 400, content_type = "json")]
+struct ErrorResponse {
+    error: String,
+}
+
+#[derive(Responder)]
+#[response(status = 200, content_type = "json")]
+struct SuccessResponse<T: rocket::serde::Serialize> {
+    data: Json<T>,
+}
+
+#[derive(serde::Serialize)]
+struct FizzBuzzResult {
+    number: i32,
+    result: String,
+}
 
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/fizzbuzz/<number>")]
+fn fizzbuzz_single(number: i32) -> SuccessResponse<FizzBuzzResult> {
+    let result = fizzbuzz(number);
+    SuccessResponse {
+        data: Json(FizzBuzzResult {
+            number,
+            result,
+        }),
+    }
+}
+
+fn fizzbuzz(number: i32) -> String {
+    if number % 3 == 0 && number % 5 == 0 {
+        "FizzBuzz".into()
+    } else if number % 3 == 0 {
+        "Fizz".into()
+    } else if number % 5 == 0 {
+        "Buzz".into()
+    } else {
+        number.to_string()
+    }
+}
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .mount("/", routes![hello, fizzbuzz_single])
 }
